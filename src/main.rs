7dbee4395d34eefb77ce7f7620c9089e73481349
Vladan Popovic <vladanovic@gmail.com>
2019-06-28 16:57:08 +0200

    Partial refactor / introduce drawing shapes

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,73 @@ use std::io::{self, Read, Write};
 use std::fs::{File};
 
 
+struct Coordinate(u32, u32);
+struct Color(u8, u8, u8);
+struct Pixel(Coordinate, Color);
+
+struct Frame {
+    buffer: Vec<u8>,
+    xoffset: u32,
+    yoffset: u32,
+    width: u32,
+    height: u32,
+    bytes_per_pixel: u32,
+}
+
+struct Shape {
+    pixels: Vec<Pixel>,
+}
+
+struct Rect {
+    min: Coordinate,
+    max: Coordinate,
+    color: Color,
+}
+
+impl Into<Shape> for Rect {
+    fn into(&self) -> Shape {
+        for i in self.min[0]..self.max[0] {
+            for j in self.min[1]..self.max[1] {
+                // add the pixels
+            }
+        }
+    }
+}
+
+impl Frame {
+    fn new(xoffset: u32, yoffset: u32, width: u32, height: u32, bytes_per_pixel: u32) -> Self {
+        Self {
+            buffer: vec![0u8; (width * height) as usize],
+            xoffset,
+            yoffset,
+            width,
+            height,
+            bytes_per_pixel,
+        }
+    }
+
+    fn draw_image(&mut self, path: &str) {
+        let img = bmp::open(path).unwrap();
+        for (x, y) in img.coordinates() {
+            let px = img.get_pixel(x, y);
+            let idx = (((y + self.yoffset) * self.width) + ((x + self.xoffset) * self.bytes_per_pixel)) as usize;
+            self.buffer[idx] = px.b;
+            self.buffer[idx + 1] = px.g;
+            self.buffer[idx + 2] = px.r;
+        }
+    }
+
+    fn draw(&mut self, shape: Shape) {
+        for Pixel((x,y), (r, g, b)) in shape.pixels {
+            let idx = (((y + self.yoffset) * self.width) + ((x + self.xoffset) * self.bytes_per_pixel)) as usize;
+            self.buffer[idx] = b;
+            self.buffer[idx + 1] = g;
+            self.buffer[idx + 2] = r;
+        }
+    }
+}
+
+
 fn read_u32_from_file(fname: &str) -> io::Result<u32> {
     let mut f = File::open(fname)?;
     let mut buffer = String::new();
@@ -30,33 +97,26 @@ fn parse_args(args: &[String]) -> Result<Option<String>, &'static str> {
     }
 }
 
+
 fn main() -> io::Result<()> {
     let args: Vec<String> = env::args().collect();
     let write_to = parse_args(&args).unwrap();
 
-    let img = bmp::open("/sys/firmware/acpi/bgrt/image").unwrap();
-    let xoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/xoffset")?;
-    let yoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/yoffset")?;
+    // Disable text mode in current tty
+    let _ = Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
 
     let mut framebuffer = Framebuffer::new("/dev/fb0").unwrap();
 
-    let h = framebuffer.var_screen_info.yres;
-    let line_length = framebuffer.fix_screen_info.line_length;
+    let xoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/xoffset")?;
+    let yoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/yoffset")?;
+    let width = framebuffer.fix_screen_info.line_length;
+    let height = framebuffer.var_screen_info.yres;
     let bytespp = framebuffer.var_screen_info.bits_per_pixel / 8;
 
-    // Disable text mode in current tty
-    let _ = Framebuffer::set_kd_mode(KdMode::Graphics).unwrap();
-    let mut frame = vec![0u8; (line_length * h) as usize];
+    let mut frame = Frame::new(xoffset, yoffset, width, height, bytespp);
+    frame.draw_image("/sys/firmware/acpi/bgrt/image");
 
-    for (x, y) in img.coordinates() {
-        let px = img.get_pixel(x, y);
-        let start_index = ((y + yoffset) * line_length + (xoffset + x) * bytespp) as usize;
-        frame[start_index] = px.b;
-        frame[start_index + 1] = px.g;
-        frame[start_index + 2] = px.r;
-    }
-
-    let _ = framebuffer.write_frame(&frame);
+    let _ = framebuffer.write_frame(frame.buffer.as_slice());
 
     let feedback = || { };
     let pass = passwd::read_pass(&feedback)?;
