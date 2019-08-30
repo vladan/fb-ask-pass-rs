6e49b9334127994ad657ba97cf3a01539c5f44b8
Vladan Popovic <vladanovic@gmail.com>
2019-08-30 18:58:26 +0200

    Render the image centered on the screen

--- src/drawing.rs
+++ src/drawing.rs
@@ -6,8 +6,8 @@ use std::io::{self, Read};
 
 pub struct Frame {
     pub buffer: Vec<u8>,
-    xoffset: u32,
-    yoffset: u32,
+    xoffset: Option<u32>,
+    yoffset: Option<u32>,
     width: u32,
     height: u32,
     bytes_per_pixel: u32,
@@ -15,8 +15,8 @@ pub struct Frame {
 
 impl Frame {
     pub fn new(fb: &Framebuffer) -> Self {
-        let xoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/xoffset").unwrap();
-        let yoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/yoffset").unwrap();
+        let xoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/xoffset").ok();
+        let yoffset = read_u32_from_file("/sys/firmware/acpi/bgrt/yoffset").ok();
         let width = fb.fix_screen_info.line_length;
         let height = fb.var_screen_info.yres;
         let bytes_per_pixel = fb.var_screen_info.bits_per_pixel / 8;
@@ -33,10 +33,18 @@ impl Frame {
 
     pub fn draw_image(&mut self, path: String) {
         let img = bmp::open(path.as_ref()).unwrap();
+        let xoffset = self
+            .xoffset
+            .unwrap_or((self.width / self.bytes_per_pixel) / 2 - img.get_width() / 2);
+        let yoffset = self
+            .yoffset
+            .unwrap_or(self.height / 2 - img.get_height() / 2);
+
         for (x, y) in img.coordinates() {
             let px = img.get_pixel(x, y);
-            let idx = (((y + self.yoffset) * self.width)
-                + ((x + self.xoffset) * self.bytes_per_pixel)) as usize;
+            let xb = (x + xoffset) * self.bytes_per_pixel;
+            let yb = (y + yoffset) * self.width;
+            let idx = (xb + yb) as usize;
             self.buffer[idx] = px.b;
             self.buffer[idx + 1] = px.g;
             self.buffer[idx + 2] = px.r;
